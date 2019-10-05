521943c7a0fc8ba89ee769017bc727cd45fb25e6
NuclearManD <dylanbrophy@gmail.com>
2019-10-05 13:29:20 -0700

    first commit - backup of work

---
+++ planemaker.py
@@ -0,0 +1,12 @@
+# I plan on fixing the bad variable
+#   names later - this is the result of my whiteboard math!
+
+Qw = None  # wing location
+Qvc = None # tail location
+Cw = None # wing chord
+Lw = None # wing length
+def get_wing_location():
+    if Qw!=None:
+        return Qw
+    else:
+        
